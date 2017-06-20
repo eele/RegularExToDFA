d3e57213bfcfaa15cf3ba2ee536f207401a0d55a
littleElectron <2727262253@qq.com>
2017-06-20 10:29:12 +0800

    add something

--- src/data/StateMatrix.java
+++ src/data/StateMatrix.java
@@ -2,4 +2,23 @@ package data;
 
 public class StateMatrix {
 	
+	private int[] matrix = null;
+	private char[] inCh = null;
+	
+	public int[] getMatrix() {
+		return matrix;
+	}
+	
+	public void setMatrix(int[] matrix) {
+		this.matrix = matrix;
+	}
+	
+	public char[] getInCh() {
+		return inCh;
+	}
+	
+	public void setInCh(char[] inCh) {
+		this.inCh = inCh;
+	}
+	
 }
--- src/ui/Graph.java
+++ src/ui/Graph.java
@@ -1,6 +1,8 @@
 package ui;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 
@@ -14,20 +16,60 @@ import javax.swing.JPanel;
  */
 public class Graph extends JPanel {
 	private static final long serialVersionUID = 1L;
-	
+
 	public Graph() {
-		this.setPreferredSize(new Dimension(680, 200));
+		this.setPreferredSize(new Dimension(710, 300));
+		this.setBackground(Color.white);
 	}
 
 	public void paint(Graphics g) {
 		super.paint(g);
-		Graphics2D g2D = (Graphics2D) g;
-		
-		paintState(g2D);
-		
+
+		paintState(g, "0", 100, 100);
+		paintState(g, "1", 200, 100);
+
+		for(int i=0;i<360;i=i+10)
+		paintLoop(g, 200, 100, i, "a");
 	}
-	
-	private void paintState(Graphics2D g2D) {
-		g2D.drawOval(100, 100, 30, 30);
+
+	/**
+	 * 绘制状态结点
+	 * 
+	 * @param g
+	 * @param num
+	 * @param x
+	 * @param y
+	 */
+	private void paintState(Graphics g, String num, int x, int y) {
+		g.drawOval(x, y, 36, 36);
+		g.setFont(new Font("宋体", Font.BOLD, 18));
+		g.drawString(num, x + 13, y + 25);
+	}
+
+	/**
+	 * 绘制环箭头
+	 * 
+	 * @param g
+	 * @param x
+	 * @param y
+	 * @param angle
+	 * @param label
+	 */
+	private void paintLoop(Graphics g, int x, int y, int angle, String label) {
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.translate(x + 18, y + 18); // 原点位置
+
+		g2d.drawString(label, (int) (Math.sin(angle * Math.PI / 180) * 60),
+				(int) (-Math.cos(angle * Math.PI / 180) * 60));
+
+		g2d.rotate(angle * Math.PI / 180);
+		g2d.drawArc(-11, -46, 20, 35, -45, 180 + 90); // 圆弧
+
+		int[] xs = { 4, 15, 6 };
+		int[] ys = { -29, -26, -15 };
+		g2d.fillPolygon(xs, ys, 3); // 实心箭头
+
+		g2d.rotate((360 - angle) * Math.PI / 180);  // 恢复旋转角度和原点位置
+		g2d.translate(-(x + 18), -(y + 18));
 	}
 }
